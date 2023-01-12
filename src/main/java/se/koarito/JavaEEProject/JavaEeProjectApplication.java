cb17f6412e255b30b2ad439edc7e9bd6acd2f40c
koarito <amiromarhonein@hotmail.fr>
2023-01-12 14:23:55 +0100

    first commit

---
+++ src/main/java/se/koarito/JavaEEProject/JavaEeProjectApplication.java
@@ -0,0 +1,13 @@
+package se.koarito.JavaEEProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/koarito/JavaEEProject/JavaEeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package se.koarito.JavaEEProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
