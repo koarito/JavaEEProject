a1a7dd0da3164b17119f4447b66fe7f68bf85d49
koarito <amiromarhonein@hotmail.fr>
2023-01-19 19:37:43 +0100

    small miss

--- src/main/java/se/koarito/JavaEEProject/JavaEeProjectApplication.java
+++ src/main/java/se/koarito/JavaEEProject/JavaEeProjectApplication.java
@@ -2,9 +2,6 @@ package se.koarito.JavaEEProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import se.koarito.JavaEEProject.data.domain.User;
-import se.koarito.JavaEEProject.repository.UserRepository;
 
 @SpringBootApplication
 public class JavaEeProjectApplication {
@@ -12,6 +9,4 @@ public class JavaEeProjectApplication {
 		SpringApplication.run(JavaEeProjectApplication.class, args);
 	}
 
-
- */
 }
