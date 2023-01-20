054b2cbca98658b3dfaaa5370a124a7f227c2c83
DenizeScheffold <denizescheffold@gmail.com>
2023-01-20 16:43:49 +0100

    appseurity config for testing

---
+++ src/main/java/se/koarito/JavaEEProject/Auth/AppSecurityConfig.java
@@ -0,0 +1,36 @@
+package se.koarito.JavaEEProject.Auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig implements WebMvcConfigurer {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests ()
+                .requestMatchers ("/", "/rest/encode", "/error", "/login", "http://localhost:8080/weather/info?latitude=31.63&longitude=-8.00").permitAll()
+                .anyRequest().authenticated ()
+                .and()
+                .formLogin();
+        return http.build();
+
+    }
+
+
+    }
+
+
