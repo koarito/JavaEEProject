c2d955102a87219c4a42699d39b3495ad83737fe
DenizeScheffold <denizescheffold@gmail.com>
2023-01-23 14:53:44 +0100

    add controller for user

--- src/main/java/se/koarito/JavaEEProject/service/WeatherService.java
+++ src/main/java/se/koarito/JavaEEProject/service/WeatherService.java
@@ -9,7 +9,7 @@ public class WeatherService {
 
  public Weather callWeatherApi(Double latitude, Double longitude){
 
- final String url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude.toString()+"&longitude="+longitude.toString()+"&hourly=temperature_2m,rain,windspeed_10m";
+ final String url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude.toString()+"&longitude="+longitude.toString()+"&current_weather=true&temperature_2m,rain,windspeed_10m";
  RestTemplate restTemplate = new RestTemplate();
  Weather weather;
 
