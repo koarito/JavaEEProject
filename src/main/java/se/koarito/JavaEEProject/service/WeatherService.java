d0a17e4049a593e39ca254bab13d7b49d6102bf4
koarito <amiromarhonein@hotmail.fr>
2023-01-23 17:28:00 +0100

    resolved conflicts

--- src/main/java/se/koarito/JavaEEProject/service/WeatherService.java
+++ src/main/java/se/koarito/JavaEEProject/service/WeatherService.java
@@ -8,8 +8,7 @@ import se.koarito.JavaEEProject.data.pojo.Weather;
 public class WeatherService {
 
  public Weather callWeatherApi(Double latitude, Double longitude){
- final String url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude.toString()+"&longitude="+longitude.toString()+"&current_weather=true&temperature_2m,rain,windspeed_10m";
- RestTemplate restTemplate = new RestTemplate();
+  final String url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude.toString()+"&longitude="+longitude.toString()+"&hourly=temperature_2m,rain,windspeed_10m,winddirection_10m"; RestTemplate restTemplate = new RestTemplate();
  Weather weather;
 
   try {
